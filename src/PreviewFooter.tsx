57fc1aad746eb568ef94b38aca9f02a173e0ddcd
tai2 <hello@tai2.net>
2022-01-24 00:41:10 +0900

    Add title

--- src/PreviewFooter.tsx
+++ src/PreviewFooter.tsx
@@ -5,7 +5,12 @@ export function PreviewFooter() {
         <div className="h-full bg-black text-sm text-gray-400 pr-3 flex items-center justify-end">
             <span>
                 boikone-editor.com created by{' '}
-                <a href="https://twitter.com/sniper_voice/" className="hover:underline">@sniper_voice</a>
+                <a
+                    href="https://twitter.com/sniper_voice/"
+                    className="hover:underline"
+                >
+                    @sniper_voice
+                </a>
             </span>
         </div>
     )
--- src/PreviewHeader.tsx
+++ src/PreviewHeader.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export function PreviewHeader() {
     return (
-        <div className="h-full bg-gradient-to-l from-fuchsia-800 to-indigo-800"></div>
+        <div className="h-full bg-gradient-to-l from-fuchsia-800 to-indigo-800 pl-3 flex items-center">
+            <h1>ボイコネエディター</h1>
+        </div>
     )
 }
