707d4c227db7ce6f93cf4c7fd73e6fe3370051c8
tai2 <hello@tai2.net>
2022-01-31 00:03:33 +0900

    Change the page name

--- src/components/PreviewFooter.tsx
+++ src/components/PreviewFooter.tsx
@@ -4,7 +4,7 @@ export function PreviewFooter() {
     return (
         <div className="flex h-full items-center justify-end bg-black pr-3 text-sm text-gray-400">
             <span>
-                wwww.boikone-editor.com created by{' '}
+                wwww.boikone-preview.com created by{' '}
                 <a
                     href="https://twitter.com/sniper_voice/"
                     className="hover:underline"
--- src/components/PreviewHeader.tsx
+++ src/components/PreviewHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function PreviewHeader() {
     return (
         <div className="flex h-full items-center bg-gradient-to-l from-fuchsia-800 to-indigo-800 pl-3">
-            <h1>ボイコネエディター</h1>
+            <h1>ボイコネプレビュー</h1>
         </div>
     )
 }
