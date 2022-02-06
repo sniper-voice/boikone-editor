2e2c45373a65209f0bbfec5e50d94109bdbaf0d1
tai2 <hello@tai2.net>
2022-02-06 10:26:53 +0900

    Add countCharacters function

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -3,6 +3,13 @@ export type Words = {
     readonly lines: Readonly<[string, ...string[]]>
 }
 
+export type CharacterCount = {
+    readonly character: string
+    readonly count: number
+}
+
 export type ScenarioText = Readonly<Words[]>
+
+export type CharacterCounts = Readonly<CharacterCount[]>
 
 export type CountByCharacter = Record<string, number>
