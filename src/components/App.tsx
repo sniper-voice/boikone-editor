339371d74dc703f6f9ab4608734d04eafd6b83ad
tai2 <hello@tai2.net>
2022-02-11 18:07:44 +0900

    Minimize the responsibility of the Preview coomponent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { parseText } from '../lib/parseText'
+import { countCharacters } from '../lib/countCharacters'
+import { aggregateCountByCharacter } from '../lib/aggregateCountByCharacter'
 import { Preview } from './Preview'
 import { Edit } from './Edit'
+import { Header } from './Header'
+import { Footer } from './Footer'
+import { Stats } from './Stats'
+import { WordsDistribution } from './WordsDistribution'
 
 type Props = {
     defaultState: {
@@ -41,11 +47,84 @@ type Props = {
 export function App({ defaultState, onStateChange }: Props) {
     const [text, setText] = useState<string>(defaultState.text)
     const scenarioText = useMemo(() => parseText(text), [text])
+    const [showStats, setShowStats] = useState<boolean>(false)
+    const characterCounts = useMemo(
+        () => countCharacters(scenarioText),
+        [scenarioText]
+    )
+    const charactersSortedByCount = useMemo(
+        () =>
+            Object.entries(aggregateCountByCharacter(characterCounts)).sort(
+                (lhs, rhs) => rhs[1] - lhs[1]
+            ),
+        [characterCounts]
+    )
+    const barColors = [
+        'bg-red-500',
+        'bg-yellow-300',
+        'bg-cyan-500',
+        'bg-purple-500',
+        'bg-lime-500',
+        'bg-red-700',
+        'bg-yellow-700',
+        'bg-cyan-700',
+        'bg-purple-700',
+        'bg-lime-700',
+    ]
+    const barColorByCharacter = charactersSortedByCount.reduce<
+        Record<string, string>
+    >((table, [character]) => {
+        table[character] =
+            character === '0'
+                ? 'bg-gray-300'
+                : barColors.shift() ?? 'bg-gray-500'
+        return table
+    }, {})
+    const entries = charactersSortedByCount.map(([character, count]) => ({
+        character,
+        count,
+        barColor: barColorByCharacter[character],
+    }))
+
+    useEffect(() => {
+        const handleDocumentClick = () => {
+            if (showStats) {
+                setShowStats(false)
+            }
+        }
+        document.addEventListener('click', handleDocumentClick)
+        return () => {
+            document.removeEventListener('click', handleDocumentClick)
+        }
+    }, [showStats])
 
     return (
         <>
             <div className="h-screen overflow-hidden">
-                <Preview scenarioText={scenarioText} />
+                <div className="relative h-full overflow-hidden bg-slate-50 text-zinc-50">
+                    <div
+                        className={`absolute right-2 top-16 ${
+                            showStats ? 'visible' : 'invisible'
+                        }`}
+                    >
+                        <Stats entries={entries} />
+                    </div>
+                    <div className="h-14">
+                        <Header onStatClick={() => setShowStats(!showStats)} />
+                    </div>
+                    <div className="h-2">
+                        <WordsDistribution
+                            characterCounts={characterCounts}
+                            barColorByCharacter={barColorByCharacter}
+                        />
+                    </div>
+                    <div className="h-[calc(100%-theme(spacing.14)-theme(spacing.2)-theme(spacing.12))]">
+                        <Preview scenarioText={scenarioText} />
+                    </div>
+                    <div className="h-12">
+                        <Footer />
+                    </div>
+                </div>
             </div>
             <Edit
                 defaultRect={{
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,11 +1,5 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react'
+import React, { useRef } from 'react'
 import { ScenarioText } from '../lib/models'
-import { countCharacters } from '../lib/countCharacters'
-import { aggregateCountByCharacter } from '../lib/aggregateCountByCharacter'
-import { Header } from './Header'
-import { Footer } from './Footer'
-import { Stats } from './Stats'
-import { WordsDistribution } from './WordsDistribution'
 import { Line } from './Line'
 
 type Props = {
@@ -13,107 +7,36 @@ type Props = {
 }
 
 export function Preview({ scenarioText }: Props) {
-    const [showStats, setShowStats] = useState<boolean>(false)
     const scrollableRef = useRef<HTMLDivElement>(null)
-    const characterCounts = useMemo(
-        () => countCharacters(scenarioText),
-        [scenarioText]
-    )
-    const charactersSortedByCount = useMemo(
-        () =>
-            Object.entries(aggregateCountByCharacter(characterCounts)).sort(
-                (lhs, rhs) => rhs[1] - lhs[1]
-            ),
-        [characterCounts]
-    )
-    const barColors = [
-        'bg-red-500',
-        'bg-yellow-300',
-        'bg-cyan-500',
-        'bg-purple-500',
-        'bg-lime-500',
-        'bg-red-700',
-        'bg-yellow-700',
-        'bg-cyan-700',
-        'bg-purple-700',
-        'bg-lime-700',
-    ]
-    const barColorByCharacter = charactersSortedByCount.reduce<
-        Record<string, string>
-    >((table, [character]) => {
-        table[character] =
-            character === '0'
-                ? 'bg-gray-300'
-                : barColors.shift() ?? 'bg-gray-500'
-        return table
-    }, {})
-    const entries = charactersSortedByCount.map(([character, count]) => ({
-        character,
-        count,
-        barColor: barColorByCharacter[character],
-    }))
-
-    useEffect(() => {
-        const handleDocumentClick = () => {
-            if (showStats) {
-                setShowStats(false)
-            }
-        }
-        document.addEventListener('click', handleDocumentClick)
-        return () => {
-            document.removeEventListener('click', handleDocumentClick)
-        }
-    }, [showStats])
 
     return (
-        <div className="relative h-full overflow-hidden bg-slate-50 text-zinc-50">
-            <div
-                className={`absolute right-2 top-16 ${
-                    showStats ? 'visible' : 'invisible'
-                }`}
-            >
-                <Stats entries={entries} />
-            </div>
-            <div className="h-14">
-                <Header onStatClick={() => setShowStats(!showStats)} />
-            </div>
-            <div className="h-2 ">
-                <WordsDistribution
-                    characterCounts={characterCounts}
-                    barColorByCharacter={barColorByCharacter}
-                />
-            </div>
-            <div
-                data-testid="preview-text"
-                className="flex h-[calc(100%-theme(spacing.14)-theme(spacing.2)-theme(spacing.12))] flex-row-reverse overflow-x-auto overflow-y-hidden bg-gray-900 px-6 pt-10"
-                onWheel={(event) => {
-                    if (!scrollableRef.current) {
-                        return
-                    }
+        <div
+            data-testid="preview-text"
+            className="flex h-full flex-row-reverse overflow-x-auto overflow-y-hidden bg-gray-900 px-6 pt-10"
+            onWheel={(event) => {
+                if (!scrollableRef.current) {
+                    return
+                }
 
-                    scrollableRef.current.scrollTo(
-                        scrollableRef.current.scrollLeft - event.deltaY * 0.5,
-                        scrollableRef.current.scrollTop
-                    )
-                }}
-                ref={scrollableRef}
-            >
-                {scenarioText
-                    .map((words, wordsIndex) =>
-                        words.lines.map((line, lineIndex) => (
-                            <Line
-                                key={`${wordsIndex}-${lineIndex}`}
-                                showCharacterName={lineIndex === 0}
-                                character={words.character}
-                                text={line}
-                            />
-                        ))
-                    )
-                    .flat()}
-            </div>
-            <div className="h-12">
-                <Footer />
-            </div>
+                scrollableRef.current.scrollTo(
+                    scrollableRef.current.scrollLeft - event.deltaY * 0.5,
+                    scrollableRef.current.scrollTop
+                )
+            }}
+            ref={scrollableRef}
+        >
+            {scenarioText
+                .map((words, wordsIndex) =>
+                    words.lines.map((line, lineIndex) => (
+                        <Line
+                            key={`${wordsIndex}-${lineIndex}`}
+                            showCharacterName={lineIndex === 0}
+                            character={words.character}
+                            text={line}
+                        />
+                    ))
+                )
+                .flat()}
         </div>
     )
 }
