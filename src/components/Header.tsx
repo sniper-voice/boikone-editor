830a3373f60207d702164e56c7801beb528dce53
tai2 <hello@tai2.net>
2022-05-29 13:36:46 +0900

    Remove the (new) label from the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ type Props = {
 export function Header({ onStatClick }: Props) {
     return (
         <div className="flex h-full items-center bg-gradient-to-l from-fuchsia-800 to-indigo-800 px-4">
-            <h1 className="grow">ボイコネプレビュー(new)</h1>
+            <h1 className="grow">ボイコネプレビュー</h1>
             <button onClick={() => onStatClick()}>
                 <ChartBarIcon className="h-5 w-5" />
             </button>
