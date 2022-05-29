b96cf6a1132098424e309bcebb4137406baf4666
tai2 <hello@tai2.net>
2022-05-29 13:39:33 +0900

    Add the reference to the source of the background image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export function Footer() {
     return (
-        <div className="flex h-full items-center bg-black px-3 text-sm text-gray-400">
+        <div className="flex h-full items-center gap-3 bg-black px-3 text-xs text-gray-400">
             <a href="?mode=classic" className="">
                 旧版
             </a>
+            <span>
+                背景画像:<a href="https://min-chi.material.jp/">みりんちえ</a>
+            </span>
             <span className="grow" />
             <span>
                 wwww.boikone-preview.com created by{' '}
